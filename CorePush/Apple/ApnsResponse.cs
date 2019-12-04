6d8a945c9109e3f396c9bd3640237692b1d619e6
Arcantel <dev@arcantel.ch>
2019-12-04 15:59:39 +0100

    - Added target framework net47  - Added target framework net462  - Return APNS response

---
+++ CorePush/Apple/ApnsResponse.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CorePush.Apple
+{
+    public class ApnsResponse
+    {
+        public bool IsSuccess { get; set;  }
+
+        public ApnsError Error { get; set; }
+    }
+
+    public class ApnsError
+    {
+        public ReasonEnum Reason {get; set;}
+        public long? Timestamp {get; set; }
+    }
+
+    /// <summary>
+    /// https://developer.apple.com/library/archive/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/CommunicatingwithAPNs.html#//apple_ref/doc/uid/TP40008194-CH11-SW15
+    /// </summary>
+    public enum ReasonEnum
+    {
+        BadCollapseId,
+        BadDeviceToken,
+        BadExpirationDate,
+        BadMessageId,
+        BadPriority,
+        BadTopic,
+        DeviceTokenNotForTopic,
+        DuplicateHeaders,
+        IdleTimeout,
+        MissingDeviceToken,
+        MissingTopic,
+        PayloadEmpty,
+        TopicDisallowed,
+        BadCertificate,
+        BadCertificateEnvironment,
+        ExpiredProviderToken,
+        Forbidden,
+        InvalidProviderToken,
+        MissingProviderToken,
+        BadPath,
+        MethodNotAllowed,
+        Unregistered,
+        PayloadTooLarge,
+        TooManyProviderTokenUpdates,
+        TooManyRequests,
+        InternalServerError,
+        ServiceUnavailable,
+        Shutdown,  
+    }
+}
