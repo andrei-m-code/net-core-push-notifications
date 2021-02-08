d501a7a6e5a33bcc68dc6f8e0a106959ec5aa0f9
Daniel Cohen Gindi <danielgindi@gmail.com>
2021-02-08 11:16:57 +0200

    `deviceId` and SenderId are optional, payload may contain a `to` or `registration_ids` already

--- CorePush/Google/FcmSender.cs
+++ CorePush/Google/FcmSender.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,21 +29,51 @@ namespace CorePush.Google
         /// https://firebase.google.com/docs/cloud-messaging/concept-options#notifications
         /// The SendAsync method will add/replace "to" value with deviceId
         /// </summary>
-        /// <param name="deviceId">Device token</param>
+        /// <param name="deviceId">Device token (will add `to` to the payload)</param>
         /// <param name="payload">Notification payload that will be serialized using Newtonsoft.Json package</param>
-        /// <exception cref="HttpRequestException">Throws exception when not successful</exception>
-        public async Task<FcmResponse> SendAsync(string deviceId, object payload)
+        /// <cref="HttpRequestException">Throws exception when not successful</exception>
+        public Task<FcmResponse> SendAsync(string deviceId, object payload)
         {
             var jsonObject = JObject.FromObject(payload);
             jsonObject.Remove("to");
             jsonObject.Add("to", JToken.FromObject(deviceId));
-            var json = jsonObject.ToString();
 
+            return SendRawAsync(jsonObject.ToString());
+        }
+
+        /// <summary>
+        /// Send firebase notification.
+        /// Please check out payload formats:
+        /// https://firebase.google.com/docs/cloud-messaging/concept-options#notifications
+        /// The SendAsync method will add/replace "to" value with deviceId
+        /// </summary>
+        /// <param name="payload">Notification payload that will be serialized using Newtonsoft.Json package</param>
+        /// <exception cref="HttpRequestException">Throws exception when not successful</exception>
+        public Task<FcmResponse> SendAsync(object payload)
+        {
+            return SendRawAsync(JsonHelper.Serialize(payload));
+        }
+
+        /// <summary>
+        /// Send firebase notification.
+        /// Please check out payload formats:
+        /// https://firebase.google.com/docs/cloud-messaging/concept-options#notifications
+        /// The SendAsync method will add/replace "to" value with deviceId
+        /// </summary>
+        /// <param name="payload">Notification payload json</param>
+        /// <exception cref="HttpRequestException">Throws exception when not successful</exception>
+        private async Task<FcmResponse> SendRawAsync(string payload)
+        {
             using (var httpRequest = new HttpRequestMessage(HttpMethod.Post, fcmUrl))
             {
-                httpRequest.Headers.Add("Authorization", $"key = {settings.ServerKey}");
-                httpRequest.Headers.Add("Sender", $"id = {settings.SenderId}");
-                httpRequest.Content = new StringContent(json, Encoding.UTF8, "application/json");
+                httpRequest.Headers.Add("Authorization", $"key={settings.ServerKey}");
+
+                if (string.IsNullOrEmpty(settings.SenderId))
+                {
+                    httpRequest.Headers.Add("Sender", $"id={settings.SenderId}");
+                }
+
+                httpRequest.Content = new StringContent(payload, Encoding.UTF8, "application/json");
 
                 using (var response = await http.SendAsync(httpRequest))
                 {
